3586377995f212e302543569192ef7ea1385e67c
Malcolm Bonar <malbonar@gmail.com>
2020-07-05 18:51:09 +1200

    adding preload of todays race cards to remove big delay on init load of that page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './shared/services/auth.service';
+import { RaceCardService } from './modules/racecard/services/RaceCardService';
+import { StateService } from './shared/services/state.service';
+import { shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +12,20 @@ import { AuthService } from './shared/services/auth.service';
 export class AppComponent implements OnInit {
   title = 'Winning Stat\'s';
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService,
+    private racecardService: RaceCardService,
+    private stateService: StateService) {}
 
   ngOnInit() {
     // On initial load, check authentication state with authorization server
     // Set up local auth streams if user is already authenticated
     this.auth.localAuthSetup();
+
+    // preload data to wake up Azure functions and reduce fetch time for common page
+    this.racecardService.fetchRaceCardMeetings().pipe(
+      shareReplay(1)
+    ).subscribe((races) => {
+      this.stateService.setTodaysRaces(races);
+    });
   }
 }
